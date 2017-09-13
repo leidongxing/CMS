7a81d8ec9b5c92b85c2183698b0b14ef16695820
leidongxing <leidongxing@gmail.com>
2017-09-13 20:05:11 +0800

    1

--- study/study/src/main/java/chapter5/config/SecurityConfig.java
+++ study/study/src/main/java/chapter5/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package chapter5.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
-@EnableWebSecurity
+//@EnableWebSecurity
+@EnableWebMvcSecurity
 public class SecurityConfig {
      
 }
