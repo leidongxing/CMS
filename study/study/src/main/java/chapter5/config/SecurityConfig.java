b5ea9132dea2b37683ba4620b5fe61c1171d9b83
leidongxing <leidongxing@gmail.com>
2017-09-16 22:45:48 +0800

    security

--- study/study/src/main/java/chapter5/config/SecurityConfig.java
+++ study/study/src/main/java/chapter5/config/SecurityConfig.java
@@ -1,11 +1,25 @@
 package chapter5.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 
 @Configuration
 //@EnableWebSecurity
 @EnableWebMvcSecurity
-public class SecurityConfig {
-     
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+//	 @Override
+//     protected void configure(HttpSecurity http) throws Exception{
+//    	 http.authorizeRequests()
+//    	 .anyRequest().authenticated()
+//    	 .and().formLogin().and().httpBasic(); 
+//     }
+	 
+	 @Override
+	 protected void configure(AuthenticationManagerBuilder auth) {
+		 auth.inMemoryAuthentication().withUser("user").password("password").roles("USER").and().withUser("admin")。password("admin").roles("USER","ADMIN");
+	 }
+	 
 }
